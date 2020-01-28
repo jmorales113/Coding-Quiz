725f9270ab9ef6b274fe863936343b881604c389
Jorge Morales <jmorales113@gmail.com>
2020-01-28 01:34:55 -0800

    Fixed undefined options.

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        question: "In what areana do the Los Angeles Lakers play in?",
+        options: ["United Center", "Madison Square Garden", "Staples Center", "United Center"],
+        answer: "Staples Center"
+    },
+    {
+        question: "How many championships have the Los Angeles Lakers won?",
+        options: ["10", "16", "12", "8"],
+        answer: "16"
+    },
+    {
+        question: "In what city did the Lakers first play in?",
+        options: ["Minneapolis", "New Orleans", "Portland", "Miami"],
+        answer: "Minneapolis"
+    },
+    {
+        question: "In what division do the Los Angeles Lakers play in?",
+        options: ["Southwest", "Northwest", "Central", "Pacific"],
+        answer: "Pacific"
+    },
+    {
+        question: "When did the Los Angeles Lakers last win an NBA championship?",
+        options: ["2003", "2010", "2008", "2005"],
+        answer: "2010"
+    }
+    
+]
